8bcffaa1fd84d2493a678bb2667391d1cebdbb3c
Camilo Sosa <ing.criscamih7@gmail.com>
2020-10-07 15:29:04 -0500

    añade amibientes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,24 +4,24 @@
 
 export const environment = {
   production: false,
-  // url_detalle_inventario: 'http://localhost:3000/api/DetalleInventario/',
-  // url_areas: 'http://localhost:3000/api/Area/',
-  // url_empleados: 'http://localhost:3000/api/Empleado/',
-  // url_estados: 'http://localhost:3000/api/Estado/',
-  // url_productos: 'http://localhost:3000/api/Producto/',
-  // url_inventario: 'http://localhost:3000/api/Inventario/',
-  // url_tipo_producto: 'http://localhost:3000/api/TipoProducto/',
-  // url_proveedor: 'http://localhost:3000/api/Proveedor/',
+  url_detalle_inventario: 'http://localhost:3000/api/DetalleInventario/',
+  url_areas: 'http://localhost:3000/api/Area/',
+  url_empleados: 'http://localhost:3000/api/Empleado/',
+  url_estados: 'http://localhost:3000/api/Estado/',
+  url_productos: 'http://localhost:3000/api/Producto/',
+  url_inventario: 'http://localhost:3000/api/Inventario/',
+  url_tipo_producto: 'http://localhost:3000/api/TipoProducto/',
+  url_proveedor: 'http://localhost:3000/api/Proveedor/',
 
   //-----depuración en local---
-  url_detalle_inventario: 'https://localhost:5001/api/DetalleInventario/',
-  url_areas: 'https://localhost:5001/api/Area/',
-  url_empleados: 'https://localhost:5001/api/Empleado/',
-  url_estados: 'https://localhost:5001/api/Estado/',
-  url_productos: 'https://localhost:5001/api/Producto/',
-  url_inventario: 'https://localhost:5001/api/Inventario/',
-  url_tipo_producto: 'https://localhost:5001/api/TipoProducto/',
-  url_proveedor: 'https://localhost:5001/api/Proveedor/',
+  // url_detalle_inventario: 'https://localhost:5001/api/DetalleInventario/',
+  // url_areas: 'https://localhost:5001/api/Area/',
+  // url_empleados: 'https://localhost:5001/api/Empleado/',
+  // url_estados: 'https://localhost:5001/api/Estado/',
+  // url_productos: 'https://localhost:5001/api/Producto/',
+  // url_inventario: 'https://localhost:5001/api/Inventario/',
+  // url_tipo_producto: 'https://localhost:5001/api/TipoProducto/',
+  // url_proveedor: 'https://localhost:5001/api/Proveedor/',
 };
 
 /*
